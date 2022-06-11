023499edda47952e2ada98805953273950117cdd
SILVA Genival <GJA1@dsone.3ds.com>
2022-06-11 11:52:32 -0300

    Begin

---
+++ src/main/java/com/captinv/captinv/CaptinvApplication.java
@@ -0,0 +1,13 @@
+package com.captinv.captinv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptinvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptinvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/captinv/captinv/CaptinvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.captinv.captinv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptinvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
